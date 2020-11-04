9993309e238fa85ec143248ba868501f37e0ec3d
HebaAliNassif <49316071+HebaAliNassif@users.noreply.github.com>
2020-11-04 17:21:40 +0200

    Screen's vertex shader and fragment shader of shape 2

---
+++ assets/shaders/fshaders/shape1.frag
@@ -0,0 +1,21 @@
+#version 330 core
+
+uniform vec2 translation;
+out vec4 frag_color;
+
+void main() {
+
+    // the center of the texture
+    vec2 center = vec2(translation.x,translation.y);
+
+    // current pixel location
+    vec2 position;
+    /*position.x= gl_FragCoord.x-1280.0/2.0;
+    position.y= gl_FragCoord.y-720.0/2.0;*/
+    position.x= gl_FragCoord.x-center.x;
+    position.y= gl_FragCoord.y-center.y;
+    //float result = pow((pow(position.x,2)+pow(position.y,2))-5000,3)-(pow(position.x,2)*pow(position.y,3));
+    float result = pow(position.x,2)+(pow(position.y-sqrt(abs(position.x)),2));
+    if (result<5000)gl_FragColor = vec4(0,1,0,1);
+
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,33 @@
 #include <application.h>
+#include <shader.h>
+#include <irrKlang.h>
+#include <iostream>
+
+using namespace irrklang;
+
+
 // This exercise Window Application that derives from "Application" parent
 class EmptyWindowApplication : public CGEngine::Application {
+    Shader program ;
+    GLuint vertex_array = 0;
+
+    glm::vec2 translation = glm::vec2(0,0);
+
 
     // This overriden function sets the window configuration params struct (title, size, isFullscreen).
+    ISoundEngine* SoundEngine = createIrrKlangDevice();
+
     CGEngine::WindowConfiguration getWindowConfiguration() override {
         return { "Empty Window", {1280, 720}, false };
     }
 
     // onInitialize() function is called once before the application loop
     void onInitialize() override {
+        //->play2D("audio/breakout.mp3", true);
+        program = Shader::LoadShader("assets/shaders/vshaders/screen.vert","assets/shaders/fshaders/shape1.frag","shape1");
 
-        //Specify the red, green, blue, and alpha values used when the color buffers are cleared.
-        //Here the clear color is Red
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-        //Not black anymore
+        glGenVertexArrays(1, &vertex_array);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     }
 
     void onImmediateGui(ImGuiIO &io) override {
@@ -25,8 +39,39 @@ class EmptyWindowApplication : public CGEngine::Application {
     void onDraw(double deltaTime) override {
         //At the start of frame we want to clear the screen. Otherwise we would still see the results from the previous frame.
         glClear(GL_COLOR_BUFFER_BIT);
-    }
+        glUseProgram(program.programID);
 
+        GLuint translation_uniform_location = glGetUniformLocation(program.programID, "translation");
+        glUniform2f(translation_uniform_location, translation.x, translation.y);
+
+
+        glBindVertexArray(vertex_array);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+    }
+    void onKeyEvent(int key, int scancode, int action, int mods) override{
+
+        if(scancode==79||scancode==2)
+        {
+            program = Shader::LoadShader("assets/shaders/vshaders/screen.vert","assets/shaders/fshaders/shape1.frag","shape1");
+        }
+        else if(scancode==80||scancode==3)
+        {
+            program = Shader::LoadShader("assets/shaders/vshaders/screen.vert","assets/shaders/fshaders/shape2.frag","shape2");
+        }
+        else if(scancode==81||scancode==4)
+        {
+            program = Shader::LoadShader("assets/shaders/vshaders/screen.vert","assets/shaders/fshaders/shape3.frag","shape3");
+        }
+        else if(scancode==75||scancode==5)
+        {
+            program = Shader::LoadShader("assets/shaders/vshaders/screen.vert","assets/shaders/fshaders/shape4.frag","shape4");
+        }
+    }
+    void onCursorMoveEvent(double x, double y) override{
+        translation.x=x;
+        translation.y=((y-720)*-1);
+    }
 };
 
 // Example Entry point
